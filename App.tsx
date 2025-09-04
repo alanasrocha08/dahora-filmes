dde16591fc09e77f1bf452dbe7ae1aecb015ef9f
alanasrocha08 <alanasr02@gmail.com>
2025-09-04 09:58:17 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ backgroundColor: "lightblue", padding: 40 }}>
+      <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
-        <View>
+        <View style={estilos.viewLogo}>
           <Text>Dá Hora Filmes!</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Text>Botão 1</Text>
+          <Text>Botão 2</Text>
+        </View>
+        <View style={estilos.viewRodape}>
+          <Text>Botão 3</Text>
+          <Text>Botão 4</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+/* Estilos */
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  viewLogo: {
+    backgroundColor: "pink",
+    flex: 2,
+  },
+  viewBotoes: {
+    backgroundColor: "lightgreen",
+    flex: 1,
+  },
+  viewRodape: {
+    backgroundColor: "lightyellow",
+    flex: 0.5,
+  },
+});
