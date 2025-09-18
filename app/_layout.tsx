eeb1aec4fb6eb71993d6857869bb1ea656332baa
alanasrocha08 <alanasr02@gmail.com>
2025-09-18 09:32:14 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,35 @@
+import { useFonts } from "expo-font";
 import { View, Text } from "react-native";
+import Loading from "../src/components/Loading";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Stack } from "expo-router";
 
 export default function Layout() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require(".../assets/fonts/Monoton-Regular.ttf"),
+  });
+  if (!fonteCarregada) {
+    return <Loading />;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#5451a6",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </SafeAreaProvider>
+  );
   return (
     <View>
       <Text>_layout</Text>
