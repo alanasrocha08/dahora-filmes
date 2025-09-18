c8fdddcf277f3d98c4ed14b36c6af5486d89b70f
alanasrocha08 <alanasr02@gmail.com>
2025-09-18 11:20:17 -0300

    .

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ import { Link, Stack } from "expo-router";
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false }} />
+      <Stack.Screen options={{ headerShown: false, headerTitle: "Home" }} />
       <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
         <View style={estilos.viewLogo}>
