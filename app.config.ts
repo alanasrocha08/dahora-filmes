693e775d7c324ddf1d6577cb6aa18270671b1f14
alanasrocha08 <alanasr02@gmail.com>
2025-10-02 09:24:59 -0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "DáHora Filmes",
   slug: "dahora-filmes",
+  owner: "alanasrocha", // Necessário para o esa
   scheme: "dahora",
   version: "1.0.0",
   orientation: "portrait",
